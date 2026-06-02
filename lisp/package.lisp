e84632be73407b24df869d5d7b8de70152022526
Elliot Bulmer <egbulmer@icloud.com>
2026-06-02 11:59:53 +0800

    Add KEY_* from raylib.h

--- lisp/package.lisp
+++ lisp/package.lisp
@@ -90,34 +90,120 @@ be compiled with `.so' files found in one location, but run with ones from anoth
 ;; to reference enum values.
 (defun keyboard-key (kw)
   (case kw
-    ;; --- Directions --- ;;
-    (:right 262)
-    (:left  263)
-    (:down  264)
-    (:up    265)
-    ;; --- Control --- ;;
-    (:space 32)
-    (:tab   258)
-    (:enter 257)
-    (:shift 340)
-    ((:esc :escape) 256)
-    ;; --- Numbers --- ;;
-    (:zero  48)
-    (:one   49)
-    (:two   50)
-    (:three 51)
-    (:four  52)
-    (:five  53)
-    (:six   54)
-    (:seven 55)
-    (:eight 56)
-    (:nine  57)
-    ;; --- Letters --- ;;
-    (:w     87)
-    (:a     65)
-    (:s     83)
-    (:d     68)
-    (:r     82)
+    (:null 0)            ;; Key: NULL used for no key pressed
+    ;; Alphanumeric keys
+    (:apostrophe 39)     ;; Key: '
+    (:comma 44)          ;; Key: 
+    (:minus 45)          ;; Key: -
+    (:period 46)         ;; Key: .
+    (:slash 47)          ;; Key: /
+    (:zero 48)           ;; Key: 0
+    (:one 49)            ;; Key: 1
+    (:two 50)            ;; Key: 2
+    (:three 51)          ;; Key: 3
+    (:four 52)           ;; Key: 4
+    (:five 53)           ;; Key: 5
+    (:six 54)            ;; Key: 6
+    (:seven 55)          ;; Key: 7
+    (:eight 56)          ;; Key: 8
+    (:nine 57)           ;; Key: 9
+    (:semicolon 59)      ;; Key: ;
+    (:equal 61)          ;; Key: =
+    (:a 65)              ;; Key: A | a
+    (:b 66)              ;; Key: B | b
+    (:c 67)              ;; Key: C | c
+    (:d 68)              ;; Key: D | d
+    (:e 69)              ;; Key: E | e
+    (:f 70)              ;; Key: F | f
+    (:g 71)              ;; Key: G | g
+    (:h 72)              ;; Key: H | h
+    (:i 73)              ;; Key: I | i
+    (:j 74)              ;; Key: J | j
+    (:k 75)              ;; Key: K | k
+    (:l 76)              ;; Key: L | l
+    (:m 77)              ;; Key: M | m
+    (:n 78)              ;; Key: N | n
+    (:o 79)              ;; Key: O | o
+    (:p 80)              ;; Key: P | p
+    (:q 81)              ;; Key: Q | q
+    (:r 82)              ;; Key: R | r
+    (:s 83)              ;; Key: S | s
+    (:t 84)              ;; Key: T | t
+    (:u 85)              ;; Key: U | u
+    (:v 86)              ;; Key: V | v
+    (:w 87)              ;; Key: W | w
+    (:x 88)              ;; Key: X | x
+    (:y 89)              ;; Key: Y | y
+    (:z 90)              ;; Key: Z | z
+    (:left-bracket 91)   ;; Key: [
+    (:backslash 92)      ;; Key: '\'
+    (:right-bracket 93)  ;; Key: ]
+    (:grave 96)          ;; Key: `
+    ;; Function keys
+    (:space 32)          ;; Key: Space
+    (:escape 256)        ;; Key: Esc
+    (:enter 257)         ;; Key: Enter
+    (:tab 258)           ;; Key: Tab
+    (:backspace 259)     ;; Key: Backspace
+    (:insert 260)        ;; Key: Ins
+    (:delete 261)        ;; Key: Del
+    (:right 262)         ;; Key: Cursor right
+    (:left 263)          ;; Key: Cursor left
+    (:down 264)          ;; Key: Cursor down
+    (:up 265)            ;; Key: Cursor up
+    (:page-up 266)       ;; Key: Page up
+    (:page-down 267)     ;; Key: Page down
+    (:home 268)          ;; Key: Home
+    (:end 269)           ;; Key: End
+    (:caps-lock 280)     ;; Key: Caps lock
+    (:scroll-lock 281)   ;; Key: Scroll down
+    (:num-lock 282)      ;; Key: Num lock
+    (:print-screen 283)  ;; Key: Print screen
+    (:pause 284)         ;; Key: Pause
+    (:f1 290)            ;; Key: F1
+    (:f2 291)            ;; Key: F2
+    (:f3 292)            ;; Key: F3
+    (:f4 293)            ;; Key: F4
+    (:f5 294)            ;; Key: F5
+    (:f6 295)            ;; Key: F6
+    (:f7 296)            ;; Key: F7
+    (:f8 297)            ;; Key: F8
+    (:f9 298)            ;; Key: F9
+    (:f10 299)           ;; Key: F10
+    (:f11 300)           ;; Key: F11
+    (:f12 301)           ;; Key: F12
+    (:left-shift 340)    ;; Key: Shift left
+    (:left-control 341)  ;; Key: Control left
+    (:left-alt 342)      ;; Key: Alt left
+    (:left-super 343)    ;; Key: Super left
+    (:right-shift 344)   ;; Key: Shift right
+    (:right-control 345) ;; Key: Control right
+    (:right-alt 346)     ;; Key: Alt right
+    (:right-super 347)   ;; Key: Super right
+    (:kb-menu 348)       ;; Key: KB menu
+    ;; Keypad keys
+    (:kp-0 320)          ;; Key: Keypad 0
+    (:kp-1 321)          ;; Key: Keypad 1
+    (:kp-2 322)          ;; Key: Keypad 2
+    (:kp-3 323)          ;; Key: Keypad 3
+    (:kp-4 324)          ;; Key: Keypad 4
+    (:kp-5 325)          ;; Key: Keypad 5
+    (:kp-6 326)          ;; Key: Keypad 6
+    (:kp-7 327)          ;; Key: Keypad 7
+    (:kp-8 328)          ;; Key: Keypad 8
+    (:kp-9 329)          ;; Key: Keypad 9
+    (:kp-decimal 330)    ;; Key: Keypad .
+    (:kp-divide 331)     ;; Key: Keypad /
+    (:kp-multiply 332)   ;; Key: Keypad *
+    (:kp-subtract 333)   ;; Key: Keypad -
+    (:kp-add 334)        ;; Key: Keypad +
+    (:kp-enter 335)      ;; Key: Keypad Enter
+    (:kp-equal 336)      ;; Key: Keypad =
+    ;; Android key buttons
+    (:back 4)            ;; Key: Android back button
+    (:menu 5)            ;; Key: Android menu button
+    (:volume-up 24)      ;; Key: Android volume up button
+    (:volume-down 25)    ;; Key: Android volume down button
     (t (error "Unknown keyboard key: ~a" kw))))
 
 #++
